fc6510a9050d2e613893df1a2418672b32af9d05
dor yakov <doryakov1@gmail.com>
2018-06-30 21:34:22 +0000

    fixed app.js again

--- app.js
+++ app.js
@@ -16,8 +16,8 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index")
     
-//var url = process.env.DATABASEURL || mongoose.connect("mongodb://localhost/yelp_camp_v12_2");
-mongoose.connect(process.env.DATABASEURL);
+var url = process.env.DATABASEURL || "mongodb://localhost/yelp_camp_v12_2"
+mongoose.connect(url);
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
