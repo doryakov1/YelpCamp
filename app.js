bf93866239a46563fd7a280996ef10e830f0e579
dor <betshean@walla.com>
2021-05-09 20:18:00 +0300

    first commit

--- app.js
+++ app.js
@@ -16,9 +16,9 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index")
     
-// var url = process.env.DATABASEURL || "mongodb://localhost/yelp_camp_v12_2"
-// mongoose.connect(url);
-mongoose.connect("mongodb://dor:123qwe123@ds221631.mlab.com:21631/yelpcampv11");
+var url = process.env.DATABASEURL || "mongodb://localhost/yelp_camp_v12_2"
+ mongoose.connect(url);
+
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
